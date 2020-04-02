bcbb145c5a262f74ac0f5a7d1bfe9cbfac42939f
Benjamin Cirmena <benji_cirmena@hotmail.com>
2020-04-02 18:04:41 +0200

    add collection & storage functions

--- envs/warehouse_env_dir/warehouse_env.py
+++ envs/warehouse_env_dir/warehouse_env.py
@@ -10,10 +10,28 @@ class Article():
     # string
     # delivery time of this article, between 0-1 smaller availability takes longer to get delivered to the warehouse
     # int
-    def __init__(self, frequency=0.5, name="article name", delivery_time=2):
+    def __init__(self, id, frequency=0.5, name="article name", delivery_time=2):
+        self.id = id
         self.frequency = frequency
         self.name = name
         self.delivery_time = delivery_time
+
+    def getName(self):
+        return self.name
+
+    def getId(self):
+        return self.id
+
+
+class ArticleCollection():
+    def __init__(self):
+        self.articles = []
+
+    def _generate_articles(self):
+        pass
+
+    def get_possible_articles(self):
+        return self.articles
 
 
 class StorageSpace():
@@ -33,12 +51,16 @@ class StorageSpace():
 
     def get_storage_space_state(self):
         # TODO return storage space info
-        pass
+        if self.article == None:
+            return 0
+        return self.article.getId()
 
 
 class Storage():
     def __init__(self, count=3):
         self.storage_spaces = self._init_spaces(count)
+        print('Initialized Storage', self.get_storage_state())
+        print(self._is_space_empty(-2))
 
     def _init_spaces(self, count):
         # TODO dynamic size
@@ -50,13 +72,32 @@ class Storage():
 
     def get_storage_state(self):
         # TODO return all states as....
-        return self.storage_spaces
+        state = []
+        for s in self.storage_spaces:
+            state.append(s.get_storage_space_state())
+        return state
+
+    def store(self, article, pos):
+        if self._is_space_empty(pos):
+            self.storage_spaces[pos].store_article(article)
+        # TODO throw error
+
+    def _is_space_empty(self, pos):
+        try:
+            return self.storage_spaces[pos].get_storage_space_state() == 0
+        except IndexError:
+            return False
 
 
 class Arrivals():
     def __init__(self, count=1):
+        self.max_arrivals = count
         self.arrivals = []
         pass
+
+    def add_article_to_arrival(self, article):
+        # TODO
+        print(self.arrivals, 3, self.max_arrivals)
 
 
 class ArrivalSpace():
@@ -110,11 +151,14 @@ class WarehouseEnv(gym.Env):
         self.max_arrivals = max_arrivals
         if seed is None:
             self.seed = random.randint(0, sys.maxsize)
+        self.arrivals = Arrivals(self.max_arrivals)
         self.possible_articles = []
         self.requests = []
-        self.stock = []
+        self.storage = Storage(3)
         print('Env initialized seed:', self.seed)
         self._add_articles()
+        #self.storage.store(Article(3, 0.5,  "Normal"), 1)
+        # print(self.storage.get_storage_state())
 
     def step(self):
         self._new_request()
@@ -138,14 +182,16 @@ class WarehouseEnv(gym.Env):
 
     def _add_articles(self):
         self.possible_articles.append(
-            Article(0.2, "Selten"))
+            Article(1, 0.2, "Selten"))
         self.possible_articles.append(
-            Article(0.8,  "Häufig"))
+            Article(2, 0.8,  "Häufig"))
         self.possible_articles.append(
-            Article(0.5,  "Normal"))
+            Article(3, 0.5,  "Normal"))
         print("Articles added...")
+    # def _add_storage_space(self):
+    #   self.stock.append(StorageSpace(1))
+    #   self.stock.append(StorageSpace(2))
+    #   self.stock.append(StorageSpace(3))
 
-    def _add_storage_space(self):
-        self.stock.append(StorageSpace(1))
-        self.stock.append(StorageSpace(2))
-        self.stock.append(StorageSpace(3))
+
+a = WarehouseEnv(None, 3, 1)
